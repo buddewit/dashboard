af420b39b4b71939750e3f144fe6a245a556c10c
buddewit <58402314+buddewit@users.noreply.github.com>
2025-12-17 18:21:42 +0100

    Update app.py

--- app.py
+++ app.py
@@ -176,11 +176,12 @@ ax_scatter.set_title("Bezetting over Tijd")
 ax_scatter.set_ylabel("Bezettingsgraad (%)")
 
 # Move the legend outside so it doesn't cover data
-sns.move_legend(ax_scatter, "lower center", bbox_to_anchor=(0.5, -0.2), ncol=3)
+sns.move_legend(ax_scatter, "lower center", bbox_to_anchor=(0.5, -0.3), ncol=4)
 
 st.pyplot(fig_scatter)
 
 # --- RENDER --
+
 
 
 
