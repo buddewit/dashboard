af83561184e0733eed2db525113b9b280c1e2373
buddewit <58402314+buddewit@users.noreply.github.com>
2025-12-17 18:37:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,8 @@ st.set_page_config(
 
 st.sidebar:
     st.title('🏂 US Population Dashboard')
+    color_theme_list = ['blues', 'cividis', 'greens', 'inferno', 'magma', 'plasma', 'reds', 'rainbow', 'turbo', 'viridis']
+    selected_color_theme = st.selectbox('Select a color theme', color_theme_list)
 
 # Set the title
 st.title('Hello Streamlit!')
@@ -190,6 +192,7 @@ sns.move_legend(ax_scatter, "lower center", bbox_to_anchor=(0.5, -0.3), ncol=4)
 st.pyplot(fig_scatter)
 
 # --- RENDER --
+
 
 
 
