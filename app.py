07e6e3b88003b9f200def9306e762923231f8947
buddewit <58402314+buddewit@users.noreply.github.com>
2026-01-30 10:03:49 +0100

    Update app.py

--- app.py
+++ app.py
@@ -51,26 +51,26 @@ with st.sidebar:
 #st.markdown('### This is a markdown')
 
 # Add a button
-if st.button('Click me'):
-    st.write('Button clicked!')
+#if st.button('Click me'):
+#    st.write('Button clicked!')
 
 # Add a slider
-value = st.slider('Select a value', 0, 100, 50)
-st.write(f'Slider value: {value}')
+#value = st.slider('Select a value', 0, 100, 50)
+#st.write(f'Slider value: {value}')
 
-range_values = st.slider(
-    "Select a range",
-    min_value=0,
-    max_value=100,
-    value=(20, 80)  # <-- tuple sets the two handles
-)
+#range_values = st.slider(
+#    "Select a range",
+#    min_value=0,
+#    max_value=100,
+#    value=(20, 80)  # <-- tuple sets the two handles
+#)
 
-st.write("Selected range:", range_values)
+#st.write("Selected range:", range_values)
 
 # Add an input box
-name = st.text_input('Enter your name')
-if name:
-    st.write(f'Hello, {name}!')
+#name = st.text_input('Enter your name')
+#if name:
+#    st.write(f'Hello, {name}!')
 
 sns.set_theme()
 
@@ -447,6 +447,7 @@ elif option == 'Elektrische autos':
     
     # ✅ Show in Streamlit
     st.pyplot(f)
+
 
 
 
